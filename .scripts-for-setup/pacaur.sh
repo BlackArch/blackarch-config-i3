e952c2259570b1979fd05cda475dd251d70852f3
i3-Arch <jamarlow410@st.nashcc.edu>
2014-02-27 16:01:40 -0500

    Update pacaur.sh

--- .scripts-for-setup/pacaur.sh
+++ .scripts-for-setup/pacaur.sh
@@ -5,7 +5,7 @@ tar xzvf cower.tar.gz &&
 sleep 1 &&
 rm cower.tar.gz &&
 cd cower &&
-makepkg -s --as-root &&
+makepkg -s --asroot &&
 sleep 2 &&
 pacman -U *.tar.xz &&
 sleep 3 &&
@@ -16,7 +16,7 @@ sleep 2 &&
 tar xzvf pacaur.tar.gz &&
 sleep 2 &&
 cd pacaur &&
-makepkg -s --as-root &&
+makepkg -s --asroot &&
 sleep 2 &&
 pacman -U *.tar.xz &&
 sleep 3 &&
